5cd20528a60d843ee331cfc1af3598117a45f9f2
baydex <dex3a.5@gmail.com>
2021-10-05 21:35:46 -0500

    middleware

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
+//Dependences
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+
+//routes
 const pokemon = require("./routes/pokemon")
 const user = require("./routes/user")
+
+//middleware
+const auth = require("./middleware/auth")
+const notFound = require("./middleware/notFound")
+const index = require("./middleware/index")
+
 
 app.use(morgan('dev'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.get("/", (req, res, next) => {
-    return res.status(200).json({ code: 1, message: "Bienvenido al pokedex"})
-})
-
-app.use("/pokemon", pokemon)
+app.get("/", index)
 app.use("/user", user)
-
-app.use((req, res, next) => {
-    return res.status(404).json({code: 404, message: "URL no encontrada"})
-})
+app.use(auth)
+app.use("/pokemon", pokemon)
+app.use(notFound)
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("server is running")
