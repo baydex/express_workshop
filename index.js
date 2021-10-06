f7a7edeb82646d4f5b10823a76804c34a90844b6
baydex <dex3a.5@gmail.com>
2021-10-05 20:55:46 -0500

    usuarios

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
-const pokemon = require("./routes/pokemon")
 const morgan = require('morgan')
+const pokemon = require("./routes/pokemon")
+const user = require("./routes/user")
 
 app.use(morgan('dev'))
 app.use(express.json());
@@ -13,6 +14,7 @@ app.get("/", (req, res, next) => {
 })
 
 app.use("/pokemon", pokemon)
+app.use("/user", user)
 
 app.use((req, res, next) => {
     return res.status(404).json({code: 404, message: "URL no encontrada"})
